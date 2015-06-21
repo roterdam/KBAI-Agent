cea1557548d0366014195a90adabc91d9f945487
Irina <muchnik.irina@gmail.com>
2015-06-21 01:49:31 -0700

    Submitted Project 1

--- Agent.py
+++ Agent.py
@@ -44,4 +44,158 @@ class Agent:
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
     def Solve(self,problem):
-        return -1
+
+        parsedObject= self.constructCorrectStructures(problem)
+        answer =self.findAnswer(parsedObject)
+
+        if len(answer)>0:
+            return answer.keys()[0]
+        else:
+            return -1
+
+    def constructCorrectStructures(self, problem):
+        problems={}
+        problems['given']={}
+        problems['solutions']={}
+        problems['problemSize']= str.split(problem.problemType, 'x')
+        newProblem ={};
+        for figure in  problem.figures:
+            if figure.isalpha():
+                self.appendProblemToCorrectStructures(problems, problem, figure, 1)
+            else:
+                 self.appendProblemToCorrectStructures(problems, problem, figure, 0)
+        newProblem=problems;
+
+        return newProblem;
+
+
+
+
+    def appendProblemToCorrectStructures(self, problems, problem, figure, isProblem):
+        if isProblem:
+            parsedProblem={problem.figures[figure].name :[]}
+            for object in  problem.figures[figure].objects:
+                parsedProblem[problem.figures[figure].name].append(problem.figures[figure].objects[object].attributes )
+            problems['given'].update(parsedProblem)
+        else:
+            parsedSolution={ problem.figures[figure].name:[]}
+            for object in  problem.figures[figure].objects:
+                parsedSolution[problem.figures[figure].name].append(problem.figures[figure].objects[object].attributes)
+            problems['solutions'].update(parsedSolution)
+
+
+
+
+    def findAnswer(self, parsedObject):
+        filteredSolution = parsedObject['solutions']
+
+        #if 2x2 matrix
+        if parsedObject['problemSize'][0] == '2' and parsedObject['problemSize'][1] == '2' :
+            #if same # of figures present
+            self.removeAnswersBasedOnSize(parsedObject, filteredSolution)
+
+            problem_A = parsedObject['given']['A']
+            problem_B = parsedObject['given']['B']
+            problem_C = parsedObject['given']['C']
+            if len(problem_A)== len(problem_B):
+                for index, shape in enumerate(problem_A):
+
+                    for attribute in shape:
+
+                        if attribute =='shape':
+                            if problem_A[index]['shape']== problem_B[index]['shape']:
+                                indexToDelete =[]
+                                for solution in filteredSolution:
+                                    if problem_C[index]['shape']==filteredSolution[solution][index]['shape']:
+                                        continue
+                                    else:
+                                        indexToDelete.append(solution);
+                                for x in indexToDelete:
+                                    filteredSolution.pop(x)
+
+                        elif attribute =='fill':
+                            if problem_A[index]['fill']== problem_B[index]['fill']:
+                                indexToDelete =[]
+                                for solution in filteredSolution:
+                                    if problem_C[index]['fill']==filteredSolution[solution][index]['fill']:
+                                        continue
+                                    else:
+                                        indexToDelete.append(solution);
+
+                                for x in indexToDelete:
+                                    filteredSolution.pop(x)
+
+                        elif attribute =='size':
+                            if problem_A[index]['size']== problem_B[index]['size']:
+                                indexToDelete =[]
+                                for solution in filteredSolution:
+                                    if problem_C[index]['size']==filteredSolution[solution][index]['size']:
+                                        continue
+                                    else:
+                                        indexToDelete.append(solution);
+
+                                for x in indexToDelete:
+                                    filteredSolution.pop(x)
+
+                        elif attribute =='angle':
+                            if 'angle' in problem_B[index] and  problem_A[index]['angle']== problem_B[index]['angle']:
+                                indexToDelete =[]
+                                for solution in filteredSolution:
+                                    if problem_C[index]['angle']==filteredSolution[solution][index]['angle']:
+                                        continue
+                                    else:
+                                        indexToDelete.append(solution);
+
+                                for x in indexToDelete:
+                                    filteredSolution.pop(x)
+                            elif 'angle' in problem_B[index] and problem_A[index]['angle']!= problem_B[index]['angle']:
+                                rotation= abs(int(problem_A[index]['angle'])- int(problem_B[index]['angle']))
+                                indexToDelete =[]
+                                for solution in filteredSolution:
+                                    if abs(int(problem_C[index]['angle'])-int(filteredSolution[solution][index]['angle']))==rotation:
+                                        continue
+                                    else:
+                                        indexToDelete.append(solution);
+
+                                for x in indexToDelete:
+                                    filteredSolution.pop(x)
+
+
+
+
+        return filteredSolution;
+
+    def removeAnswersBasedOnSize(self,parsedObject,filteredSolution):
+        problem_A = parsedObject['given']['A']
+        problem_B = parsedObject['given']['B']
+        problem_C = parsedObject['given']['C']
+        indexToDelete=[]
+        if  len(problem_A)== len(problem_B):
+            #remove all solutions that miss cases number
+            for solution in filteredSolution:
+                if len(problem_C)==len(filteredSolution[solution]):
+                    continue
+                else:
+                    indexToDelete.append(solution)
+
+            for x in indexToDelete:
+                filteredSolution.pop(x)
+        elif len(problem_A)>len(problem_B):
+            for solution in filteredSolution:
+                if len(problem_C)>len(filteredSolution[solution]):
+                    continue
+                else:
+                    indexToDelete.append(solution);
+
+            for x in indexToDelete:
+                filteredSolution.pop(x)
+
+        elif len(problem_A)<len(problem_B):
+            for solution in filteredSolution:
+                if len(problem_C)>len(filteredSolution[solution]):
+                    continue
+                else:
+                    indexToDelete.append(solution)
+
+            for x in indexToDelete:
+                filteredSolution.pop(x)
